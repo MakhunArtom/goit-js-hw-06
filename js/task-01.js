8b68b6cc37892a94ddd1019f3e323efd10406f3c
MakhunArtom <super_makhun@ukr.net>
2022-08-20 16:39:45 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,30 +1,5 @@
-// Кількість КАТЕГОРІЙ \\\\\\\\\\\\\
-const categoriesAmountRef = document.querySelectorAll(".item");
-console.log(`Number of categories: ${categoriesAmountRef.length}`);
-
-// // Масив ЗАГОЛОВКІВ //////////////
-// const titlRef = document.querySelectorAll("h2");
-
-// // Категорія ANIMAL \\\\\\\\\\\\\
-// const animalLiRef = categoriesAmountRef[0].querySelectorAll("li");
-
-// console.log(`Category: ${titlRef[0].textContent}`);
-// console.log(`Elements: ${animalLiRef.length}`);
-
-// // Категорія products \\\\\\\\\\\\\
-// const productsLiRef = categoriesAmountRef[1].querySelectorAll("li");
-
-// console.log(`Category: ${titlRef[1].textContent}`);
-// console.log(`Elements: ${productsLiRef.length}`);
-
-// // Категорія technologies \\\\\\\\\\\\\
-// const technologiesLiRef = categoriesAmountRef[2].querySelectorAll("li");
-
-// console.log(`Category: ${titlRef[2].textContent}`);
-// console.log(`Elements: ${technologiesLiRef.length}`);
-
-// Другий СПОСІБ \\\\\\\\\
 const itemEl = document.querySelectorAll(".item");
+console.log(`Number of categories: ${itemEl.length}`);
 
 itemEl.forEach(function (element) {
   console.log(`Category: ${element.firstElementChild.textContent}`);
