fe6ba94992242bc5c8e2dae2e23b6711af0fe6ea
MakhunArtom <super_makhun@ukr.net>
2022-08-20 16:29:14 +0300

    task-2 complit

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const createlement = (araay) => {
+  return araay.map((element) => {
+    const liEl = document.createElement("li");
+    liEl.classList.add("item");
+    liEl.textContent = element;
+    return liEl;
+  });
+};
+
+const ulEl = document.querySelector("#ingredients");
+ulEl.prepend(...createlement(ingredients));
