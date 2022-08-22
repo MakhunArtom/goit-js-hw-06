0e2d67da98cfb7c9caa9a63fb473f177de7d5962
MakhunArtom <super_makhun@ukr.net>
2022-08-22 18:22:56 +0300

    Task-8 complet

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
+
+  if (form.email.value === "" || form.password.value === "") {
+    alert("Заполните все поля");
+  } else {
+    formData.forEach((value, name) => {
+      console.log(`${name}: ${value} `);
+    });
+  }
+
+  form.reset();
+}
